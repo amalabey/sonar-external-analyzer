b1405dc8761120d8fdaf0793112e068c03fe1d0e
Amal Abeygunawardana <amalabey@gmail.com>
2022-11-06 07:25:22 +0000

    Generate sonar rules for checkov rules

--- tools/rules-generator/main.py
+++ tools/rules-generator/main.py
@@ -1,10 +1,14 @@
+from checkov import CheckovRulesProvider
 from horusec import HorusecRulesProvider
 from sonarqube import SonarQubeRulesXmlWriter
 
 if __name__ == "__main__":
     print("Generating rules from external tools documentation")
     horusec = HorusecRulesProvider()
-    rules = horusec.get_rules()
+    checkov = CheckovRulesProvider()
+    hsc_rules = horusec.get_rules()
+    ckv_rules = checkov.get_rules()
+    rules = hsc_rules + ckv_rules
 
     writer = SonarQubeRulesXmlWriter()
     writer.write_rules(rules, "../../src/main/resources/sonar-rules.xml")
